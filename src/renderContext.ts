872cb391bc244147d879f5e590ff3ba6a3d83f79
Evin Sellin <evinism@gmail.com>
2022-04-17 23:37:56 -0700

    incremental progress towards using rendercontexts rather than dom storage

--- src/renderContext.ts
+++ src/renderContext.ts
@@ -1,6 +1,12 @@
-import { BlorpNodeConstructor, UseStateHandler } from './types';
+import { BlorpNodeConstructor, Optional, UseStateHandler } from './types';
 import { patch, text, elementOpen, elementClose, elementVoid, currentElement } from 'incremental-dom-evinism';
 import { HookDomain } from './hookDomain';
+
+type RenderContext = {
+  hookDomain: HookDomain;
+  parent: Optional<RenderContext>;
+  childrenContexts: {[key: string]: RenderContext[]};
+};
 
 export class RenderTreeContext {
   rootElement: Element;
@@ -11,7 +17,9 @@ export class RenderTreeContext {
     this.rootNode = rootNode;
   }
 
-  _renderNode = (nodeConstructor: BlorpNodeConstructor, hookDomain: HookDomain) => {
+  _renderNode = (nodeConstructor: BlorpNodeConstructor, renderContext: RenderContext) => {
+    const hookDomain = renderContext.hookDomain;
+
     hookDomain.enter(this.render);
     const node = nodeConstructor({ rerender: this.render });
     hookDomain.exit();
@@ -19,36 +27,47 @@ export class RenderTreeContext {
     if (typeof node === 'string') {
       text(node);
     } else if(node.type === 'element') {
+      const nodeKey = node.key;
       const props = Object.entries(node.props).flat();
       if (node.children) {
         elementOpen(node.tag, node.key, [], ...props);
         // Try to make contexts work!
         const element = currentElement() as any;
-        if (!element._blorp_hook_domains) {
-          element._blorp_hook_domains = [];
+        if (!element._blorp_render_contexts) {
+          element._blorp_render_contexts = [];
         }
 
         for (let i = 0;  i < node.children.length; i++) {
           const child = node.children[i];
-          if (!element._blorp_hook_domains[i]) {
-            element._blorp_hook_domains[i] = new HookDomain();
+          if (!element._blorp_render_contexts[i]) {
+            let newRenderContext: RenderContext = {
+              hookDomain: new HookDomain(),
+              parent: renderContext,
+              childrenContexts: {},
+            };
+            element._blorp_render_contexts[i] = newRenderContext;
           }
-          this._renderNode(child, element._blorp_hook_domains[i]);
+          this._renderNode(child, element._blorp_render_contexts[i]);
         }
         elementClose(node.tag);
       } else {
         elementVoid(node.tag, node.key, null, props);
       }
     } else if(node.type === 'fragment') {
+      const nodeKey = node.key;
       for (let child of node.children) {
         // TODO: try to make hook domains work here. It's not clear what the best solution is.
-        this._renderNode(child, hookDomain);
+        this._renderNode(child, renderContext);
       }
     }
   }
 
   render = () => {
-    const baseHookDomain = new HookDomain();
-    patch(this.rootElement, () => this._renderNode(this.rootNode, baseHookDomain));
+    const baseRenderContext: RenderContext = {
+      hookDomain: new HookDomain(),
+      parent: undefined,
+      childrenContexts: {},
+    };
+    patch(this.rootElement, () => this._renderNode(this.rootNode, baseRenderContext));
   }
 }
